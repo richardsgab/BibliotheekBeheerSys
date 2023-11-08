19ddf3edf0661eba32c63a8d268078e6d7e9c379
Your Name <you@example.com>
2023-11-08 12:28:18 +0100

    Finish all methods. But Add methos still not working

--- Bib/Program.cs
+++ Bib/Program.cs
@@ -9,20 +9,20 @@ namespace Bib
 {
     internal class Program
     {
-        static string[] bookTitles = { "Book1", "Book2"};
-        static string[] bookAuthors = {"Author1", "Author2"};
+        static string[] bookTitles = { "Book1", "Book2" };
+        static string[] bookAuthors = { "Author1", "Author2" };
         static string[] magsNames = { "Mag1", "Mag2" };
-        static string[] users = {"User1", "User2"};
+        static string[] users = { "User1", "User2" };
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            
+
             string input;
             string title = null;
-            string author = null;           
+            string author = null;
+            string searchItem = null;
             string name = null;
-            string zoekterm = null;
-                       
+
 
             Console.WriteLine("BIBLIOTHEEK"); //Menu title
 
@@ -32,7 +32,7 @@ namespace Bib
                 input = Console.ReadLine();
 
 
-                switch (input) 
+                switch (input)
                 {
                     case "1":
                         ShowMaterials(bookTitles, bookAuthors, magsNames);
@@ -41,20 +41,20 @@ namespace Bib
                         AddMaterial(title, author);
                         break;
                     case "3":
-                        DeleteMaterial(title);
+                        DeleteMaterial(ref bookTitles, ref magsNames, title);
                         break;
                     case "4":
-                        ZoekMateriaal(zoekterm);
+                        SearchMaterial(bookTitles, bookAuthors, magsNames, searchItem);
                         break;
                     case "5":
-                        RegistreerGebruiker(name);
+                        UserRegister(name);
                         break;
                 }
                 Console.WriteLine("Press any key to return to the main menu.");
                 Console.ReadKey();
                 Console.Clear();
             }
-            
+
         }
         public static void ShowMenu() // Shows the Main Menu
         {
@@ -64,9 +64,9 @@ namespace Bib
             Console.WriteLine("4. Search for a book or a mag");
             Console.WriteLine("5. New user register");
         }
-        public static void AddMaterial(string title, string author)//Add material
+        public static void AddMaterial(string title, string author)//Add material.Both for books and mags
         {
-            
+
             Console.WriteLine("1 - Book  ");
             Console.WriteLine("2 - Revue  ");
             int choiceMat = Int32.Parse(Console.ReadLine());
@@ -76,67 +76,150 @@ namespace Bib
             string[] magsNames = { "Mag1", "Mag2" };
 
             if (choiceMat == 1)
-            {                
+            {
                 Console.WriteLine("Enter the title: ");
                 title = Console.ReadLine();
-
                 //Add a new place at the end of the array to fill it:
                 Array.Resize(ref bookTitles, bookTitles.Length + 1);
                 //Puts the new entry in the new place created:
                 bookTitles[bookTitles.Length - 1] = title;
-                
+
                 Console.WriteLine("Enter the author: ");
-                author = Console.ReadLine();
                 Array.Resize(ref bookAuthors, bookAuthors.Length + 1);
+                author = Console.ReadLine();
                 bookAuthors[bookAuthors.Length - 1] = author;
-                
+
             }
             else if (choiceMat == 2)
-            {               
+            {
                 Console.WriteLine("Enter the title: ");
                 title = Console.ReadLine();
                 Array.Resize(ref magsNames, magsNames.Length + 1);
-                magsNames[magsNames.Length - 1] = title;          
-            }        
-            
+                magsNames[magsNames.Length - 1] = title;
+            }
+
         }
-       
-        public static void ShowMaterials(string[] bookTitles, string[] boekAuthors, string[] magsNames) //Show all books and mags
-        {     
-           
+
+        public static void ShowMaterials(string[] bookTitles, string[] bookAuthors, string[] magsNames) //Show all books and mags
+        {
+
             Console.WriteLine("1 - Book");
             Console.WriteLine("2 - Revue");
             int choiceMat = Int32.Parse(Console.ReadLine());
 
-            if (choiceMat == 1) 
+            if (choiceMat == 1)
             {
                 for (int i = 0; i < bookTitles.Length; i++)
                 {
-                    Console.WriteLine($"Book id: {i + 1} \n Title: {bookTitles[i]} \n Author: {boekAuthors[i]}");
-                }                
+                    if (bookTitles[i] == "9999")//Doesen't show the item already convert to "9999"
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Book id: {i + 1} \n Title: {bookTitles[i]} \n Author: {bookAuthors[i]}");
+                    }
+                }
             }
 
             if (choiceMat == 2)
             {
                 for (int i = 0; i < magsNames.Length; i++)
                 {
-                    Console.WriteLine($"Mag id: {i + 1} \n Title: {magsNames[i]}");
+                    if (magsNames[i] == "9999")//Doesen't show the item already convert to "9999"
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Mag id: {i + 1} \n Title: {magsNames[i]}");
+                    }
                 }
             }
 
         }
-        static void DeleteMaterial(string title)
+        static void DeleteMaterial(ref string[] bookTitles, ref string[] magsNames, string title) //Delete
         {
+            Console.WriteLine("What material do you want to delete? : ");
+            Console.WriteLine("1 - Book  ");
+            Console.WriteLine("2 - Revue  ");
+            int choiceMat = Int32.Parse(Console.ReadLine());
+
+            if (choiceMat == 1)
+            {
+                Console.WriteLine("Enter the title: ");
+                title = Console.ReadLine();
+                // Find the index of the book title:
+                int index = Array.IndexOf(bookTitles, title);
+                if (index >= 0)
+                {
+                    bookTitles[index] = "9999";
+                }
+                else
+                {
+                    Console.WriteLine("Title not found.");
+                }
+            }
+            else if (choiceMat == 2)
+            {
+                Console.WriteLine("Enter the title: ");
+                title = Console.ReadLine();
+                int index = Array.IndexOf(magsNames, title);
+                if (index >= 0)
+                {
+                    magsNames[index] = "9999";
+                }
+                else
+                {
+                    Console.WriteLine("Title not found.");
+                }
+            }
+        }
+        static void SearchMaterial(string[] bookTitles, string[] bookAuthors, string[] magsNames, string searchItem)//Search in the arrays for a search term (both titles or authors)
+        {
+            Console.WriteLine("1 - Book");
+            Console.WriteLine("2 - Revue");
+            int choiceMat = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the search term: ");
+            searchItem = Console.ReadLine();
+            if (choiceMat == 1)
+            {
+                if (Array.IndexOf(bookTitles, searchItem) >= 0)
+                {
+                    Console.WriteLine($"'{searchItem}' found in Books Titles.");
+                }
+                else if (Array.IndexOf(bookAuthors, searchItem) >= 0)
+                {
+                    Console.WriteLine($"'{searchItem}' found in Books Authors.");
+                }
+                else
+                {
+                    Console.WriteLine("Search term not found.");
+                }
+            }
+            else if (choiceMat == 2)
+            {
+                if (Array.IndexOf(magsNames, searchItem) >= 0)
+                {
+                    Console.WriteLine($"'{searchItem}' found in Magazines Titles.");
+                }
+                else
+                {
+                    Console.WriteLine("Search term not found.");
+                }
+            }
 
         }
-        static void ZoekMateriaal(string zoekterm)
+        static void UserRegister(string name)//Add a new User Name to the users array
         {
+            Console.WriteLine("Enter your User name");
+            name = Console.ReadLine();
+
+            Array.Resize(ref users, users.Length + 1);
+            users[users.Length - 1] = name;
+            Console.WriteLine($" User {name}. You are succesfully added to our Library!");
 
         }
-        static void RegistreerGebruiker(string naam)
-        {
-
-        }
-        
     }
 }
